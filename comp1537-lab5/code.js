90152f74c4e4b47cfb2b96ea3035cad31d0fd203
Setareh Nouri <smouri3@my.bcit.ca>
2022-02-26 13:27:10 -0800

    added lab5

---
+++ comp1537-lab5/code.js
@@ -0,0 +1,11 @@
+function calculate_area() {
+    console.log("calculate_area () got called")
+    r = parseInt(jQuery("#x").val()) ;
+    jQuery("#p1").html(r * r * 22/7)
+
+}
+
+function setup() {
+    jQuery("#calc").click(calculate_area);
+}
+jQuery(document).ready(setup);
