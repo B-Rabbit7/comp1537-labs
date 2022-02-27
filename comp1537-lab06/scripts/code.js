229f79ddf528c75cb37792bc4438f129610a7911
Setareh Nouri <smouri3@my.bcit.ca>
2022-02-27 13:46:02 -0800

    finished switch function

--- comp1537-lab06/scripts/code.js
+++ comp1537-lab06/scripts/code.js
@@ -1,64 +1,121 @@
+// function add() {
+//     //grab first operand
+//     f = parseInt($("#x").val());
+//     //grab second operand
+//     s = parseInt($("#y").val());
+//     $("#answer").html(f + ' + ' + s + " = " + (f + s));
+//     old = f + ' + ' + s + " = " + (f + s);
+//     $("#history").append('<span class = "add">' + old + '</span><br>');
+// }
 
-function add() {
+// function sub() {
+//     //grab first operand
+//     f = parseInt($("#x").val());
+//     //grab second operand
+//     s = parseInt($("#y").val());
+//     $("#answer").html(f + ' - ' + s + ' = ' + (f - s));
+//     old = f + ' - ' + s + ' = ' + (f - s);
+//     $("#history").append('<span class = "sub">' + old + '</span><br>');
+
+// }
+
+// function multiply() {
+//     //grab first operand
+//     f = parseInt($("#x").val());
+//     //grab second operand
+//     s = parseInt($("#y").val());
+//     $("#answer").html(f + ' * ' + s + ' = ' + (f * s));
+//     old = f + ' * ' + s + ' = ' + (f * s);
+//     $("#history").append('<span class = "mul">' + old + '</span><br>');
+
+// }
+
+// function divide() {
+//     //grab first operand
+//     f = parseInt($("#x").val());
+//     //grab second operand
+//     s = parseInt($("#y").val());
+//     $("#answer").html(f + ' / ' + s + ' = ' + (f / s));
+//     old = f + ' / ' + s + ' = ' + (f / s);
+//     $("#history").append('<span class = "div">' + old + '</span><br>');
+// }
+
+// function IncreaseFont() {
+//     f = parseInt($("#history").css("font-size"));
+//     a = $("#history")
+//     f = f + 10 + 'px'
+//     a.css({
+//         "font-size": f
+//     })
+// }
+
+// function DecreaseFont() {
+//     f = parseInt($("#history").css("font-size"));
+//     a = $("#history")
+//     f = f - 10 + 'px'
+//     a.css({
+//         "font-size": f
+//     })
+// }
+
+function calculator(operation) {
     //grab first operand
     f = parseInt($("#x").val());
     //grab second operand
     s = parseInt($("#y").val());
-    $("#answer").html(f + ' + ' + s + " = " + (f+s));
-    old = f + ' + ' + s + " = " + (f+s);
-    $("#history").append('<span class = "add">' + old + '</span><br>');
-}
-function sub(){
-    //grab first operand
-    f = parseInt($("#x").val());
-    //grab second operand
-    s = parseInt($("#y").val());
-    $("#answer").html(f + ' - ' + s + ' = ' + (f-s));
-    old = f + ' - ' + s + ' = ' + (f-s);
-    $("#history").append('<span class = "sub">' + old + '</span><br>');
+    //switch operation
+    switch (operation) {
+        case "add":
+            $("#answer").html(f + ' + ' + s + " = " + (f + s));
+            old = f + ' + ' + s + " = " + (f + s);
+            $("#history").append('<span class = "add">' + old + '</span><br>');
+            break;
+        case "sub":
+            $("#answer").html(f + ' - ' + s + ' = ' + (f - s));
+            old = f + ' - ' + s + ' = ' + (f - s);
+            $("#history").append('<span class = "sub">' + old + '</span><br>');
+            break;
+        case "multiply":
+            $("#answer").html(f + ' * ' + s + ' = ' + (f * s));
+            old = f + ' * ' + s + ' = ' + (f * s);
+            $("#history").append('<span class = "mul">' + old + '</span><br>');
+            break;
+        case "divide":
+            $("#answer").html(f + ' / ' + s + ' = ' + (f / s));
+            old = f + ' / ' + s + ' = ' + (f / s);
+            $("#history").append('<span class = "div">' + old + '</span><br>');
+            break;
+        case "F-up":
+            f = parseInt($("#history").css("font-size"));
+            a = $("#history");
+            f = f + 10 + 'px'
+            a.css({
+                "font-size": f
+            })
+            break;
+        case "F-down":
+            f = parseInt($("#history").css("font-size"));
+            a = $("#history");
+            f = f - 10 + 'px'
+            a.css({
+                "font-size": f
+            })
+            break;
+        default:
+            console.log("error");
+    }
 
 }
 
-function multiply(){
-    //grab first operand
-    f = parseInt($("#x").val());
-    //grab second operand
-    s = parseInt($("#y").val());
-    $("#answer").html(f + ' * ' + s + ' = ' + (f*s));
-    old = f + ' * ' + s + ' = ' + (f*s);
-    $("#history").append('<span class = "mul">' + old + '</span><br>');
-
-}
-function divide(){
-    //grab first operand
-    f = parseInt($("#x").val());
-    //grab second operand
-    s = parseInt($("#y").val());
-    $("#answer").html(f + ' / ' + s + ' = ' + (f/s));
-    old = f + ' / ' + s + ' = ' + (f/s);
-    $("#history").append('<span class = "div">' + old + '</span><br>');
-}
-function IncreaseFont(){
-    f = parseInt($("#history").css("font-size"));
-    a = $("#history")
-    f = f+  10 + 'px'
-    a.css({"font-size": f})
-}
-
-function DecreaseFont(){
-    f = parseInt($("#history").css("font-size"));
-    a = $("#history")
-    f = f- 10 + 'px'
-    a.css({"font-size": f})
-}
-
-
-function setup() {
-    $("#add").click(add);
-    $("#sub").click(sub);
-    $("#multiply").click(multiply);
-    $("#divide").click(divide);
-    $("#F-up").click(IncreaseFont)
-    $("#F-down").click(DecreaseFont)
-}
-jQuery(document).ready(setup);
+    function setup() {
+        // $("#add").click(add);
+        // $("#sub").click(sub);
+        // $("#multiply").click(multiply);
+        // $("#divide").click(divide);
+        // $("#F-up").click(IncreaseFont)
+        // $("#F-down").click(DecreaseFont)
+        $(".button").click(function () {
+            calculator(this.id);
+        });
+    }
+    jQuery(document).ready(setup);
