b29c07e945b5bf130649d871ce4545432d8fe774
Setareh Nouri <smouri3@my.bcit.ca>
2022-03-27 00:14:00 -0700

    finished a2

--- comp1537-assign2/code.js
+++ comp1537-assign2/code.js
@@ -1,42 +1,15 @@
-current_page_id = null;
+current_page_id = 1;
 results_array = null;
 max_page = null;
 page_size = null;
+last_movie = 3;
+//last page needs to be global
 
 
-// function success_data(data) {
-//     console.log(data);
-//     $('.numbered').show();
-//     $("#first").show();
-//     $("#last").show();
-//     for (let i = 0; i < data.results.length; i++) {
-//         x = data.results[i].original_title + '<br>';
-//         y = data.results[i].overview + '<br>';
-//         image = data.results[i].poster_path;
-//         z = `<img src="http://image.tmdb.org/t/p/w500/${image}">`
-        
-//         h = `<input type="submit" class= "expand" id=${data.results[i].backdrop_path} value="View Poster"></input>`
-
-//         $("#main").append('<ol><li>' + x + y + z +  '</li></ol>'+ h + '<hr>');
-
-        
-//         $('#page_size').change(console.log($('#page_size').val()));
-
-//     }
-//     page_size = parseInt($('#page_size').val());
-//     // max page id = math ceil, starts with 1
-//     max_page_id = Math.ceil(data.results.length/page_size);
-//     console.log("page id is :" + max_page_id);
-//     starting_index = page_size * (max_page_id - 1)
-//     console.log(starting_index)
-
-// 
-function display(current_page_id, page_size){
-
-}
 function paginate_buttons(){
     $("#page_buttons").empty();
-    console.log("results array: " + results_array);
+    page_size = Number($("option:selected").val());
+    // console.log(results_array);
     max_page = Math.ceil(results_array.results.length / page_size);
     for(i=1; i<= max_page; i++){
         x = `<button id = "${i}" class = "numbered">${i}</button>`
@@ -46,28 +19,79 @@ function paginate_buttons(){
     $("#last").show();
 }
 
-function process_data(data){
+function populate_array(data){
     results_array = data;
-    for(i=0; i < data.results.length; i++){
-        x = data.results[i].original_title + '<br>';
-        y = data.results[i].overview + '<br>';
-        image = data.results[i].poster_path;
+    show_movies1();
+}
+
+// function show_movies(data){
+//     //empty the page before populating new material
+//     $("#main").empty();
+//     $("#expand").empty();
+//     //set data to an array so we can access the items in it easier
+//     results_array = data;
+//     //get the page size value from list size
+//     page_size = Number($("option:selected").val());
+//     //determine the index of the first movie to be listed according to page size
+//     first_movie = (current_page_id - 1) * page_size;
+//     //determine the index of the last movie to be listed according to page size
+//     last_movie = current_page_id * page_size - 1;
+//     //loop through the array according to respective indices
+//     for(i = first_movie; i <= last_movie; i++){
+//         //get the movie title according to i
+//         x = results_array.results[i].original_title + '<br>';
+//         //get the movie description according to i
+//         y = results_array.results[i].overview + '<br>';
+//         //get the movie poster according to i
+//         image = results_array.results[i].poster_path;
+//         //get the image of the poster from source
+//         z = `<img src="http://image.tmdb.org/t/p/w500/${image}">`
+//         //populate the section with a button to show the backdrop poster once cliked - expand class
+//         h = `<input type="submit" class= "expand" id=${results_array.results[i].backdrop_path} value="View Poster"></input>`
+//         //append all items needed to the html section with main id
+//         $("#main").append( + x + y + z + h + '<hr>');
+//     }
+//     paginate_buttons();
+// }
+
+//function to run without repopulating results_array
+function show_movies1(){
+    //empty the page before populating new material
+    $("#main").empty();
+    $("#expand").empty();
+    //set data to an array so we can access the items in it easier
+    // results_array = data;
+    //get the page size value from list size
+    page_size = Number($("option:selected").val());
+    //determine the index of the first movie to be listed according to page size
+    first_movie = (current_page_id - 1) * page_size;
+    //determine the index of the last movie to be listed according to page size
+    last_movie = current_page_id * page_size - 1;
+    //loop through the array according to respective indices
+    for(i = first_movie; i <= last_movie; i++){
+        //get the movie title according to i
+        x = (i+1 + '. ' ) + results_array.results[i].original_title + '<br>';
+        //get the movie description according to i
+        y = results_array.results[i].overview + '<br>';
+        //get the movie poster according to i
+        image = results_array.results[i].poster_path;
+        //get the image of the poster from source
         z = `<img src="http://image.tmdb.org/t/p/w500/${image}">`
-        
-        h = `<input type="submit" class= "expand" id=${data.results[i].backdrop_path} value="View Poster"></input>`
-
-        $("#main").append('<ol><li>' + x + y + z +  '</li></ol>'+ h + '<hr>');
+        //populate the section with a button to show the backdrop poster once cliked - expand class
+        h = `<input type="submit" class= "expand" id=${results_array.results[i].backdrop_path} value="View Poster"></input>`
+        //append all items needed to the html section with main id
+        $("#main").append( x + y + z + h + '<hr>');
     }
-
     paginate_buttons();
 }
+
 function get_movie_data() {
     //ajax
     y = $("#movie_name").val();
     $.ajax({
         "url": `https://api.themoviedb.org/3/search/movie?api_key=ed4ef9b0f9bcb9c237ab83a2c2ffb909&language=en-US&page=1&query=${y}`,
         "type": "GET",
-        "success": process_data
+        "success": populate_array
 
     })
     $("#page_buttons").show();
@@ -75,50 +99,59 @@ function get_movie_data() {
 
 function expand(){
     g = $(this).attr("id");
-    console.log("this is g:"+ g)
+    console.log( g)
     $("#expand").html(`<img src="https://image.tmdb.org/t/p/original${g}" width = "400%">`)
 }
 function test(){
     x = $(this).attr("id")
-    $("#result").html(`<h1> Display(${x}, ${page_size})</h1>`)
     current_page_id = Number(x);
+    show_movies1();
     $("#prev").show();
     $("#next").show();
 }
 
 function first(){
-    $("#result").html(`<h1> Display(1, ${page_size})</h1>`)
+    current_page_id = 1
+    show_movies1();
+    // $("#result").html(`<h1> Display(${current_page_id}, ${page_size})</h1>`)
 }
 function last(){
-    $("#result").html(`<h1> Display(7, ${page_size})</h1>`)
+    current_page_id = Math.ceil(results_array.results.length / page_size);
+    show_movies1();
+    // $("#result").html(`<h1> Display(${current_page_id}, ${page_size})</h1>`)
 }
 function prev(){
     if(current_page_id > 1)
         current_page_id--;
-        $("#result").html(`<h1> Display(${current_page_id}, ${page_size}) </h1>`);
+    show_movies1();
+        // $("#result").html(`<h1> Display(${current_page_id}, ${page_size}) </h1>`);
 
 }
 
 function next(){
-    if(current_page_id < 7)
+    if(current_page_id < Math.ceil(results_array.results.length / page_size))
         current_page_id++;
-    $("#result").html(`<h1> Display(${current_page_id}, ${page_size}) </h1>`);
-    display(current_page_id, page_size);   
+    show_movies1();
+    // $("#result").html(`<h1> Display(${current_page_id}, ${page_size}) </h1>`);  
 }
 function page_size_changed(){
-    page_size  = Number($(this).val())
+    current_page_id = 1;
+    console.log(results_array)
+    page_size  = Number($(this).val());
     paginate_buttons();
+    show_movies1();
 }
 
 function setup() {
+    //executed when the user presses the searchmovies button
     $("#searchmovie").click(get_movie_data);
+    //exdcutes when the user wants to see the backdrop image of a movie
     $('body').on("click", ".expand", expand);
-    // console.log($('#page_size').val());
-    // $('#page_size').change(page_size_change);
+    //executes when user clicks on a paginated button with numbered class
     $('body').on("click", ".numbered", test)
     $("#first").click(first);
     $("#last").click(last);
-    $('.numbered').hide();
+    // $('.numbered').hide();
     $("#first").hide();
     $("#last").hide();
     $("#prev").click(prev);
@@ -126,10 +159,8 @@ function setup() {
     $("#prev").hide();
     $("#next").hide();
     $("#page_buttons").hide();
-
     // making the page change dynamic
-    $("select").change(page_size_changed);
-    page_size = Number($("option:selected").val());
+    $("#page_size").change(page_size_changed);
 
 }
 $(document).ready(setup)
